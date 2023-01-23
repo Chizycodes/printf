9ee0df3244649b51b9703970f74350a649998b56
chizycodes <okwudirejoy@gmail.com>
2023-01-23 15:27:47 -0800

    printf fix

--- _printf.c
+++ _printf.c
@@ -32,7 +32,7 @@ int run_printf(const char *format, va_list args, buffer_t *output)
 	unsigned int (*f)(va_list, buffer_t *,
 			unsigned char, int, int, unsigned char);
 
-	for (i = 0; *(format + 1); i++)
+	for (i = 0; *(format + i); i++)
 	{
 		len = 0;
 		if (*(format + i) == '%')
@@ -86,7 +86,7 @@ int _printf(const char *format, ...)
 
 	va_start(args, format);
 
-	ret = run_print(format, args, output);
+	ret = run_printf(format, args, output);
 
 	return (ret);
 }
